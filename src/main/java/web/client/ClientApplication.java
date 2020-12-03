e54453eef3b3bd1983c9087c786dee276432e741
Maksim <mgorbiik@mail.ru>
2020-12-03 19:31:56 +0200

    Start point

--- src/main/java/web/client/ClientApplication.java
+++ src/main/java/web/client/ClientApplication.java
@@ -9,7 +9,7 @@ public class ClientApplication {
     public static void main(String[] args) {
         System.out.println(new User(1,"1234","234234","Name","Surname","Patronymic",
                 "Address","email","Roles"));
-       // SpringApplication.run(ClientApplication.class, args);
+        SpringApplication.run(ClientApplication.class, args);
     }
 
 }
