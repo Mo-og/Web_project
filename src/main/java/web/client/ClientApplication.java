49d3341c99edc60783d8add7a4140e2cd2048fae
Maksim <mgorbiik@mail.ru>
2020-12-03 19:32:09 +0200

    Start point

--- src/main/java/web/client/ClientApplication.java
+++ src/main/java/web/client/ClientApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClientApplication {
 
     public static void main(String[] args) {
-        System.out.println(new User(1,"1234","234234","Name","Surname","Patronymic",
-                "Address","email","Roles"));
         SpringApplication.run(ClientApplication.class, args);
     }
 
